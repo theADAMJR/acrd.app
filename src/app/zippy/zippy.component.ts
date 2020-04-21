8933b1014747b8d0a46706c8bd3a0b69cb0dbd1b
theADAMJR <adamjf123@gmail.com>
2020-04-21 14:51:42 +0100

    add zippy transition

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,9 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { style, state, trigger, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'zippy',
   templateUrl: './zippy.component.html',
-  styleUrls: ['./zippy.component.css']
+  styleUrls: ['./zippy.component.css'],
+  animations: [ 
+    trigger('expandCollapse', [
+      state('collapsed', style({
+        height: 0,
+        padding: '0 20px'
+      })),
+      state('expanded', style({ height: '*' })),
+      transition('collapsed <=> expanded', [ animate('200ms ease') ])
+    ])
+  ]
 })
 export class ZippyComponent {
     expanded = false;
