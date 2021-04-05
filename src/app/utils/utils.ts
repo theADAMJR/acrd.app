eea2635e0c3d8fa0daecdd25cdb51b33c6ac0b19
ADAMJR <adamjf123@gmail.com>
2021-04-05 10:39:14 +0100

    home tweak

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -45,7 +45,7 @@ export function uuid() {
     .floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);  
-  return new Array(8)
+  return 'u' + new Array(8)
     .fill('')
     .map(s4)
     .join('');  
