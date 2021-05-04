86df35e77680d0489358a9536d76cd964c904717
Your Name <you@example.com>
2021-05-04 18:42:10 +0100

    page works offline

---
+++ src/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('fetch', function(event) {
+  event.respondWith(async function() {
+     try{
+       var res = await fetch(event.request);
+       var cache = await caches.open('cache');
+       cache.put(event.request.url, res.clone());
+       return res;
+     }
+     catch(error){
+       return caches.match(event.request);
+      }
+    }());
+});
