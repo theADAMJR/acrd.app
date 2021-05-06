e763d63fd0468f8ac0a28d359df9686303e6e30c
= <adamjf123@gmail.com>
2021-05-06 16:53:10 +0100

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   endpoint: 'http://localhost:3000/api/v1',
   rootEndpoint: 'http://localhost:3000',
   githubURL: 'https://github.com/accorddotapp',
-  version: 'v0.1.8a',
+  version: 'v0.1.9a',
 };
 
 /*
