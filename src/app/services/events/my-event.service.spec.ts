0a7b8dc7dbe64e63288ae917ca4c5739e66f234f
= <adamjf123@gmail.com>
2021-05-08 23:53:32 +0100

    finish my event tests

--- src/app/services/events/my-event.service.spec.ts
+++ src/app/services/events/my-event.service.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppModule } from 'src/app/app.module';
+import { ChannelTypes } from 'src/app/types/entity-types';
 import { AccordMock } from 'src/tests/accord-mock';
+import { ChannelService } from '../channel.service';
 import { GuildService } from '../guild.service';
 import { UserService } from '../user.service';
-
 import { MyEventService } from './my-event.service';
 
 describe('MyEventService', () => {
   let service: MyEventService;
+  let channelService: ChannelService;
   let guildService: GuildService;
   let userService: UserService;
 
@@ -17,21 +19,35 @@ describe('MyEventService', () => {
       .compileComponents();
 
     service = TestBed.inject(MyEventService);
-    userService = TestBed.inject(UserService);
+    channelService = TestBed.inject(ChannelService);
     guildService = TestBed.inject(GuildService);
+    userService = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('add friends, dm channel added');
+  it('add friends, dm channel added', () => {
+    const dmChannel = AccordMock.channel(
+      AccordMock.snowflake(),
+      { type: 'DM' },
+    ) as ChannelTypes.DM;
+    
+    service.addFriend({
+      friend: AccordMock.user(),
+      sender: AccordMock.user(),
+      dmChannel,
+    });
+
+    expect(channelService.channels).toContain(dmChannel);    
+  });
 
   it('update friends, sender and friend updated', () => {
     const friend = AccordMock.user();
     const sender = AccordMock.user();
 
-    service.addFriend({
+    service.updateFriends({
       sender: {
         ...sender,
         friendRequestIds: [friend._id],
@@ -67,5 +83,13 @@ describe('MyEventService', () => {
     service.updatePresence({ userId: user._id, status: 'OFFLINE' });
 
     expect(user.status).toEqual('OFFLINE');
+  });
+
+  it('update user, self user updated', async () => {
+    userService.self = AccordMock.self();
+
+    service.updateUser({ partialUser: { username: 'epic-user' } });
+
+    expect(userService.self.username).toEqual('epic-user');
   });
 });
--- src/app/services/events/my-event.service.ts
+++ src/app/services/events/my-event.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserTypes } from 'src/app/types/entity-types';
 import { ChannelService } from '../channel.service';
 import { GuildService } from '../guild.service';
 import { PingService } from '../ping.service';
@@ -43,14 +44,17 @@ export class MyEventService {
     const user = this.userService.self;
     this.userService.upsert(user._id, partialUser);
 
-    if ('lastReadMessages' in partialUser) {
-      for (const channelId in partialUser.lastReadMessages) {
-        const channel = await this.channelService.getAsync(channelId);
-        const messageId = partialUser.lastReadMessages[channelId];
+    if ('lastReadMessages' in partialUser)
+      await this.updateReadMessages(partialUser);
+  }
 
-        if (channel?.lastMessageId === messageId)
-          this.pings.markAsRead(channelId);
-      }
+  private async updateReadMessages(partialUser: Partial<UserTypes.Self>) {
+    for (const channelId in partialUser.lastReadMessages) {
+      const channel = await this.channelService.getAsync(channelId);
+      const messageId = partialUser.lastReadMessages[channelId];
+
+      if (channel?.lastMessageId === messageId)
+        this.pings.markAsRead(channelId);
     }
   }
 }
