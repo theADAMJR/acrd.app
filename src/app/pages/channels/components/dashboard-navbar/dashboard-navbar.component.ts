bcdbaff1f6a9e5e501bf51b10fb7e23348e3a197
= <adamjf123@gmail.com>
2021-05-13 19:05:29 +0100

    fix friend list navbar

--- src/app/pages/channels/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/pages/channels/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'dashboard-navbar',
   templateUrl: './dashboard-navbar.component.html',
-  styleUrls: ['./dashboard-navbar.component.css']
+  styleUrls: ['./dashboard-navbar.component.css'],
 })
 export class DashboardNavbarComponent {
   @Input() public hasToggle = false;
