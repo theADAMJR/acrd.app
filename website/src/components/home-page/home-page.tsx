b4a88b5388599bc46d3242e1618aaab007d8f211
ADAMJR <adamjf123@gmail.com>
2021-07-26 19:29:42 +0100

    Update Navbar Styles

--- website/src/components/home-page/home-page.tsx
+++ website/src/components/home-page/home-page.tsx
@@ -3,12 +3,17 @@ import './home-page.scoped.css';
 
 const HomePage: React.FunctionComponent = () => {
   return (
-    <>
+    <div>
       <Navbar />
       <main>
-        <h1></h1>
+        <section>
+          <h1>It's time to ditch Skype and TeamSpeak.</h1>
+          <p className="lead">All-in-one voice and text chat.</p>
+          <button className="bg-success text-dark">Login</button>
+          <button className="bg-success text-dark">Open</button>
+        </section>
       </main>
-    </>
+    </div>
   );
 }
  
--- website/src/components/navbar/navbar.tsx
+++ website/src/components/navbar/navbar.tsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
+import './navbar.scoped.css';
  
 const Navbar: React.FunctionComponent = () => {
   return (
-    <nav className="mx-2 navbar d-inline">
+    <nav className="flex justify-between p-4">
       <a className="logo">
-        <span className="text-bold text-black">DCLONE</span>
-        <span className="text-muted mx-1">|</span>
-        <span className="text-thin">ESSENTIAL</span>
+        <span className="text-bold text-white">DCLONE</span>
+        <span className="text-gray-600 mx-1">|</span>
+        <span className="text-thin text-muted">ESSENTIAL</span>
       </a>
-      <div className="d-inline float-right">
-        <Link to="/login">Login</Link>
+      <div>
+        <button className="rounded-full ring ring-gray-400 px-4 py-1">
+          <Link to="/login">Login</Link>
+        </button>
       </div>
     </nav>
   );
