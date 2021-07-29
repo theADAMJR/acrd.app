f12491dab0fd297b77637a8d8d3b2e12fdc4a0a9
ADAMJR <adamjf123@gmail.com>
2021-07-29 22:58:55 +0100

    REAL snowflake generation

--- backend/src/utils/snowflake.ts
+++ backend/src/utils/snowflake.ts
@@ -1,8 +1,30 @@
-import SnowflakeId from 'snowflake-id';
+import cluster from 'cluster';
 
-const snowflake = new SnowflakeId({
-  mid : 42,
-  offset : (2019-1970)*31536000*1000
-});
+let inc = 0;
+let lastSnowflake: string;
+const accordEpoch = 1609459200000;
 
-export const generate = (): string => snowflake.generate();
+export function generate() {
+  const msSince = (new Date().getTime() - accordEpoch)
+    .toString(2)
+    .padStart(42, '0');  
+  const pid = process.pid
+    .toString(2)
+    .slice(0, 5)
+    .padStart(5, '0');
+  const wid = (cluster.worker?.id ?? 0)
+    .toString(2)
+    .slice(0, 5)
+    .padStart(5, '0');
+  const getInc = (add: number) => (inc + add)
+    .toString(2)
+    .padStart(12, '0');
+  
+  let snowflake = `0b${msSince}${wid}${pid}${getInc(0)}`;
+  (snowflake === lastSnowflake)
+    ? snowflake = `0b${msSince}${wid}${pid}${getInc(1)}`
+    : inc = 0;  
+
+  lastSnowflake = snowflake;   
+  return BigInt(snowflake).toString();
+}
