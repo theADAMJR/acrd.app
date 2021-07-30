aa15f5081d2c35a8052bd7c20a1428866514cbaf
ADAMJR <adamjf123@gmail.com>
2021-07-30 17:55:10 +0100

    Add SessionManager

--- backend/src/ws/events/guild-create.ts
+++ backend/src/ws/events/guild-create.ts
@@ -1,7 +1,16 @@
 import { WSEvent } from './ws-event';
+import { Socket } from 'socket.io';
+import { WS } from '../websocket';
+import { Guild } from '../../data/guild';
 
 export default class implements WSEvent<'GUILD_CREATE'> {
   public on = 'GUILD_CREATE' as const;
 
-  
+  public async invoke({ io, sessions }: WS, client: Socket, params: Params.GuildCreate) {
+    const ownerId = sessions.get(client.id);
+    
+    io.emit('GUILD_CREATE', {
+      guild: await Guild.create({ name, ownerId }),
+    } as Args.GuildCreate);
+  }
 }
