724608215a8cf897892b85cf0642c019f769db44
ADAMJR <adamjf123@gmail.com>
2021-07-30 17:31:44 +0100

    Create Guild Modal

--- frontend/src/components/modals/create-guild.tsx
+++ frontend/src/components/modals/create-guild.tsx
@@ -5,9 +5,35 @@ import { closeModal } from '../../store/ui';
 const CreateGuild: React.FunctionComponent = (props) => {
   const dispatch = useDispatch();
   const openModal = useSelector((s: Store.AppStore) => s.ui.openModal);
+
+  const style: any = {
+    overlay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(0, 0, 0, 0.75)'
+    },
+    content: {
+      position: 'absolute',
+      top: '40px',
+      left: '40px',
+      right: '40px',
+      bottom: '40px',
+      border: '1px solid #ccc',
+      background: '#fff',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      borderRadius: '4px',
+      outline: 'none',
+      padding: '20px'
+    }
+  };
   
   return (
     <ReactModal
+      style={style}
       className="bg-bg-tertiary w-1/2 flex self-center"
       appElement={document.querySelector('#root')!}
       isOpen={openModal === CreateGuild.name}
