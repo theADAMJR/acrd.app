c706cc0f969e7bea9809f189ccabd39994ac5628
ADAMJR <adamjf123@gmail.com>
2021-07-31 04:34:01 +0100

    Add Dropdown

--- frontend/src/components/navigation/sidebar/sidebar-content.tsx
+++ frontend/src/components/navigation/sidebar/sidebar-content.tsx
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
-import { faChevronDown, faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 import SidebarFooter from './sidebar-footer';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Dropdown from '../../utils/dropdown';
 
 import './sidebar-content.scoped.css';
 
@@ -15,24 +16,24 @@ const SidebarContent: React.FunctionComponent = () => {
       style={{height: '34px'}}
       to={`/channels/${ui.activeGuild!.id}/${c.id}`}
       className={`
-        cursor-pointer flex items-center rounded p-2
+        cursor-pointer flex items-center rounded p-2 pl-3
         ${c.id === ui.activeChannel?.id && 'active'}`}>
       <FontAwesomeIcon
-        className="float-left mr-1 scale-150"
+        className="float-left mr-2 scale-150 muted fill-current"
         icon={faHashtag} />
       <span>{c.name}</span>
     </Link>
   ));
   
+  // guild sidebar content
   return (
     <div className="flex flex-col sidebar-content bg-bg-secondary">
-      <div className="sidebar-header">
-        <div className="inline-flex justify-content-between">
-          {ui.activeGuild && (<>
-            <h1 className="flex-grow font-bold">{ui.activeGuild.name}</h1>
-            <FontAwesomeIcon icon={faChevronDown} />
-          </>)}
-        </div>
+      <div className="sidebar-header pl-2.5 pr-4">
+        {ui.activeGuild && (
+          <Dropdown title={ui.activeGuild.name}>
+            
+          </Dropdown>
+        )}
       </div>
       <div className="sidebar-details flex-grow px-2">
         <div className="h-4" />
