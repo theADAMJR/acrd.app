e1fcb602e6019a5902bdd0f53afd955bedb82175
ADAMJR <adamjf123@gmail.com>
2021-07-31 22:53:45 +0100

    Add Big Boys

---
+++ frontend/src/components/pages/guild-settings-page.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from './page-wrapper';
+
+const GuildSettingsPage: React.FunctionComponent = () => {
+  return (
+    <PageWrapper>
+      <h1>Guild Settings</h1>
+    </PageWrapper>
+  );
+}
+ 
+export default GuildSettingsPage;
---
+++ frontend/src/components/pages/user-settings-page.tsx
@@ -0,0 +1,11 @@
+import PageWrapper from './page-wrapper';
+
+const UserSettingsPage: React.FunctionComponent = () => {
+  return (
+    <PageWrapper>
+      <h1>User Settings</h1>
+    </PageWrapper>
+  );
+}
+ 
+export default UserSettingsPage;
