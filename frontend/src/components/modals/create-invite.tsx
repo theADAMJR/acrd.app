6b052215bddecdc4a68414f3098bc3d692c71023
ADAMJR <adamjf123@gmail.com>
2021-07-31 12:04:43 +0100

    .

--- frontend/src/components/modals/create-invite.tsx
+++ frontend/src/components/modals/create-invite.tsx
@@ -1,22 +1,20 @@
 import { useEffect } from 'react';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import ReactModal from 'react-modal';
-import { useDispatch, useSelector, useStore } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createInvite } from '../../store/guilds';
 import { closedModal } from '../../store/ui';
 
 const CreateInvite: React.FunctionComponent = () => {
   const dispatch = useDispatch();
-  const { register } = useForm();
-  const ui = useSelector((s: Store.AppStore) => s.ui);
-  const store = useStore();
-
-  const [inviteCode, setInviteCode] = useState();
-  const guild = ui.activeGuild!;
+  const { register, setValue } = useForm();
+  const { activeGuild, activeInvite, openModal } = useSelector((s: Store.AppStore) => s.ui);
 
   useEffect(() => {
-    dispatch(createInvite(guild.id));
+    if (!activeGuild) return;
+
+    dispatch(createInvite(activeGuild.id));
+    setValue('inviteCode', activeInvite?.id);
   }, []);
 
   const style: any = {
@@ -25,16 +23,18 @@ const CreateInvite: React.FunctionComponent = () => {
       backgroundColor: 'rgba(0, 0, 0, 0.75)'
     },
   };
+
+  if (!activeGuild || !activeInvite) return null;
   
   return (
     <ReactModal
       style={style}
       className="overflow-auto absolute bg-bg-tertiary w-1/3 inset-x-1/3 inset-y-3/4 p-5 rounded-lg outline-none"
       appElement={document.querySelector('#root')!}
-      isOpen={ui.openModal === CreateInvite.name}
+      isOpen={openModal === CreateInvite.name}
       onRequestClose={() => dispatch(closedModal())}>
       <header className="mb-5">
-        <h1 className="font-bold inline uppercase">Invite Friends to {guild.name}</h1>
+        <h1 className="font-bold inline uppercase">Invite Friends to {activeGuild.name}</h1>
       </header>
 
       <div className="mt-10" />
