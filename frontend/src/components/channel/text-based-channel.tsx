6394e19195d469b4428547fe40aa321c5b085ba7
ADAMJR <adamjf123@gmail.com>
2021-08-04 21:30:09 +0100

    Simplify Text Based Channel

--- frontend/src/components/channel/text-based-channel.tsx
+++ frontend/src/components/channel/text-based-channel.tsx
@@ -2,38 +2,22 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchMessages, getChannelMessages } from '../../store/messages';
 import Message from './message/message';
 import MessageBox from './message-box/message-box';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import './text-based-channel.scoped.css';
-import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 import { useEffect } from 'react';
+import TextChannelHeader from './text-channel-header';
  
 const TextBasedChannel: React.FunctionComponent = () => {
   const dispatch = useDispatch();
-  const channel = useSelector((s: Store.AppStore) => s.ui.activeChannel)!;  
+  const channel = useSelector((s: Store.AppStore) => s.ui.activeChannel)!;
   const messages = useSelector(getChannelMessages(channel.id));
 
   useEffect(() => {    
     dispatch(fetchMessages(channel.id));
   }, [messages.length]); // only fetches channel messages when not cached
-
-  const welcome = (
-    <div className="m-4 pb-6 border-bottom">
-      <span className="rounded-full heading">
-        <FontAwesomeIcon
-          style={{ color: 'var(--muted)' }}
-          icon={faHashtag}
-          size="3x" />
-      </span>
-      <h1 className="text-3xl font-bold my-2">Welcome to #{channel.name}!</h1>
-      <p className="lead">This is the start of the #{channel.name} channel.</p>
-    </div>
-  );
   
   return (
     <div className="text-based-channel flex flex-col flex-grow">
-      <div className="messages overflow-auto mb-5 mr-1 mt-1">
-        {welcome}
+      <div className="messages overflow-auto mb-5 mr-1 mt-1 flex-grow">
+        <TextChannelHeader />
         {messages.map(m => <Message key={m.id} message={m} />)}
       </div>
       <MessageBox />
---
+++ frontend/src/components/channel/text-channel-header.tsx
@@ -0,0 +1,22 @@
+import { faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useSelector } from 'react-redux';
+
+const TextChannelHeader: React.FunctionComponent = () => {
+  const channel = useSelector((s: Store.AppStore) => s.ui.activeChannel)!;
+  
+  return (
+    <div className="m-4 pb-6 border-bottom">
+      <span className="rounded-full heading">
+        <FontAwesomeIcon
+          style={{ color: 'var(--muted)' }}
+          icon={faHashtag}
+          size="3x" />
+      </span>
+      <h1 className="text-3xl font-bold my-2">Welcome to #{channel.name}!</h1>
+      <p className="lead">This is the start of the #{channel.name} channel.</p>
+    </div>
+  );
+}
+
+export default TextChannelHeader;
