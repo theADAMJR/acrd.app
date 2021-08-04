b07f647c4831bedfb6027ecf97793116cab26cd1
ADAMJR <adamjf123@gmail.com>
2021-08-04 22:14:22 +0100

    Clean Sidebar Icon

--- frontend/src/components/navigation/sidebar/sidebar-icon.tsx
+++ frontend/src/components/navigation/sidebar/sidebar-icon.tsx
@@ -14,25 +14,31 @@ export interface SidebarIconProps {
 const SidebarIcon: React.FunctionComponent<SidebarIconProps> = ({ guildId, imageURL, name }) => {  
   const activeGuild = useSelector((s: Store.AppStore) => s.ui.activeGuild);
   const location = useLocation();
-  const isActive = activeGuild?.id === guildId
-    || location.pathname === '/channels/@me' && !guildId;
 
   if (imageURL)
     imageURL = `${environment.rootAPIURL}${imageURL}`;
   
-  const abbr = getAbbr(name);
-  const icon = (imageURL)
-    ? <img style={{borderRadius: 'inherit'}} className="h-12 w-12" src={imageURL} alt={name} />
-    : <span className="heading h-12 w-12 flex items-center justify-center">{abbr}</span>;
+  const Icon = () => (imageURL)
+    ? <img
+        style={{borderRadius: 'inherit'}}
+        className="h-12 w-12"
+        src={imageURL}
+        alt={name} />
+    : <span className="heading h-12 w-12 flex items-center justify-center">{getAbbr(name)}</span>;
   
+  const isActive = activeGuild?.id === guildId
+    || location.pathname.startsWith('/channels/@me') && !guildId;
+
   const activeClasses = (isActive)
     ? 'rounded-xl bg-primary'
     : 'rounded-full bg-bg-primary';
     
   return (
     <div className={`wrapper ${isActive && 'active'}`}>
-      <div className="selected rounded bg-white absolute -left-1" />
-      <div className={`cursor-pointer guild-icon flex justify-center mb-2 ${activeClasses}`}>{icon}</div>
+      <div className="selected h-0 w-0 rounded bg-white absolute -left-1" />
+      <div className={`cursor-pointer guild-icon flex justify-center mb-2 ${activeClasses}`}>
+        <Icon />
+      </div>
     </div>
   );
 }
