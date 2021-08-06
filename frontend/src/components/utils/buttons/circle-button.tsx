43b289e1ec72b243c92d0e9d9e4c65b7cdab9b53
ADAMJR <adamjf123@gmail.com>
2021-08-06 20:11:42 +0100

    Refactor Links

--- frontend/src/components/utils/buttons/circle-button.tsx
+++ frontend/src/components/utils/buttons/circle-button.tsx
@@ -1,6 +1,6 @@
 const CircleButton: React.FunctionComponent<any> = (props) => {
   return (
-    <button className="rounded-full ring ring-gray-400 px-4 py-1">{props.children}</button>
+    <button className="rounded-full ring ring-gray-400 secondary px-4 py-1">{props.children}</button>
   );
 }
  
