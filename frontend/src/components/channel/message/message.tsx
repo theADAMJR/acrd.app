1c97c645d357f5463416c1470d95a5292efceb35
ADAMJR <adamjf123@gmail.com>
2021-08-10 17:04:14 +0100

    Improve Edited Indicator

--- frontend/src/components/channel/message/message.tsx
+++ frontend/src/components/channel/message/message.tsx
@@ -32,12 +32,12 @@ const Message: React.FunctionComponent<MessageProps> = ({ message }: MessageProp
   }
 
   const leftSide = () => {
-    const time = moment(createdAt).format('HH:mm');
-
     return (isExtra())
-      ? <span className="timestamp text-xs">{time}</span>
+      ? <span className="timestamp text-xs">
+          {moment(createdAt).format('HH:mm')}
+        </span>
       : <img
-          className="rounded-full cursor-pointer"
+          className="rounded-full cursor-pointer w-10 h-10"
           src={`${environment.rootAPIURL}${author.avatarURL}`}
           alt={author.username} />;
   }
@@ -50,28 +50,24 @@ const Message: React.FunctionComponent<MessageProps> = ({ message }: MessageProp
       [1]: 'Yesterday',
       [0]: 'Today',
       [-1]: 'Tomorrow',
-     }[days];
+    }[days];
+    const timestamp = (day) ? `[${day}] [at] HH:mm` : 'DD/MM/YYYY';
 
-    const format = (day)
-      ? `[${day}] [at] HH:mm`
-      : 'DD/MM/YYYY';
-
-    const timestamp = moment(createdAt).format(format);
-
-    return (<>
-      <span className="text-base heading hover:underline cursor-pointer mr-2">{author.username}</span>
-      <span className="text-xs">{timestamp}</span>
-    </>);
+    return (
+      <>
+        <span className="text-base heading hover:underline cursor-pointer mr-2">{author.username}</span>
+        <span className="text-xs">{moment(createdAt).format(timestamp)}</span>
+      </>
+    );
   }
 
-  const isEditing = editingMessageId === message.id;
-
-  const MessageContent = () => (isEditing)
+  const MessageContent = () => (editingMessageId === message.id)
     ? <MessageBox
         content={message.content}
         editingMessageId={message.id} />
     : <div className="relative">
-        <div className="normal whitespace-pre-wrap">{message.content}{message.updatedAt && <span>(edited)</span>}</div>
+        <div className="normal whitespace-pre-wrap">{message.content}{message.updatedAt &&
+          <span className="select-none muted edited text-xs ml-1">(edited)</span>}</div>
       </div>;
 
   const messageClass = `message flex ${!isExtra() && 'mt-4'}`;
@@ -79,7 +75,7 @@ const Message: React.FunctionComponent<MessageProps> = ({ message }: MessageProp
   return (
     <div className={messageClass}>
       <div className="left-side text-xs w-16 mr-2 pl-5 pt-1">{leftSide()}</div>
-      <div className="relative message-content flex-grow">
+      <div className="relative flex-grow px-2">
         <div className="absolute toolbar right-0 -mt-3 z-10">
           <MessageToolbar message={message} />
         </div>
