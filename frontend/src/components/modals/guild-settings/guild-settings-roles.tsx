ac2664296182221e6faba4b97fa736a73ca53370
ADAMJR <adamjf123@gmail.com>
2021-08-27 14:19:40 +0100

    Add Tailwind CSS Breakpoints

--- frontend/src/components/modals/guild-settings/guild-settings-roles.tsx
+++ frontend/src/components/modals/guild-settings/guild-settings-roles.tsx
@@ -1,16 +1,49 @@
 import { useForm } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { PermissionTypes } from '../../../services/perm-service';
+import { getGuildRoles } from '../../../store/guilds';
  
-const GuildSettingsRoles: React.FunctionComponent = () => {
-  const guild = useSelector((s: Store.AppState) => s.ui.activeGuild)!;
-  const dispatch = useDispatch();
-  const { handleSubmit, setValue } = useForm();
+const GuildSettingsRoles: React.FunctionComponent = () => {  
+  const permsForm = useForm();
+  const { guildId }: any = useParams();
+  const roles = useSelector(getGuildRoles(guildId));
+  
+  const getPermissions = (): number  => {
+    const value = permsForm.getValues();
+    
+    let permissions = 0;
+    for (const formGroupName in value)
+      for (const key in value[formGroupName]) {
+        const hasPermission = value[formGroupName][key];
+        permissions |= (hasPermission)
+          ? PermissionTypes.General[key] || PermissionTypes.Text[key] || PermissionTypes.Voice[key]
+          : 0;
+      }
+    return permissions;
+  }
+  
+  const description = {
+    general: {
+      'ADMINISTRATOR': `Gives all permissions. This is a dangerous permission.`,
+      'CREATE_INVITE': 'Ability to create invites for users to join this guild.',
+      'KICK_MEMBERS': 'Ability to kick members from this guild.',
+      'MANAGE_CHANNELS': 'Ability to create, edit, or delete channels.',
+      'MANAGE_GUILD': `Ability to edit general guild settings.`,
+      'MANAGE_ROLES': 'Ability to manage guild roles.',
+      'VIEW_CHANNELS': 'Ability to view channels.',
+    },
+    text: {
+      'MANAGE_MESSAGES': `Ability to manage message other member's messages.`,
+      'READ_MESSAGES': `Ability to read messages,`,
+      'SEND_MESSAGES': 'Ability to send messages in text channels.',
+    },
+  };
 
   return (
-    <form className="flex flex-col pt-14 px-10 pb-20 h-full mt-1">
-      <header>
-        <h1 className="text-xl font-bold inline">Roles</h1>
-      </header>
+    <form className="grid grid-cols-12 flex flex-col pt-14 px-10 pb-20 h-full mt-1">
+      <div className="lg:col-span-3 col-span-12">roles list</div>
+      <div className="lg:col-span-9 col-span-12">edit role</div>
     </form>
   );
 }
--- types/permission-types.ts
+++ types/permission-types.ts
@@ -1,29 +1,29 @@
 export namespace PermissionTypes {
   export enum General {
     VIEW_CHANNELS = 1024,
-    MANAGE_NICKNAMES = 512,
-    CHANGE_NICKNAME = 256,
+    // MANAGE_NICKNAMES = 512,
+    // CHANGE_NICKNAME = 256,
     CREATE_INVITE = 128,
     KICK_MEMBERS = 64,
-    BAN_MEMBERS = 32,
+    // BAN_MEMBERS = 32,
     MANAGE_CHANNELS = 16,
     MANAGE_ROLES = 8,
     MANAGE_GUILD = 4,
-    VIEW_AUDIT_LOG = 2,
-    ADMINISTRATOR = 1
+    // VIEW_AUDIT_LOG = 2,
+    ADMINISTRATOR = 1,
   }
   export enum Text {
-    ADD_REACTIONS = 2048 * 16,
-    MENTION_EVERYONE = 2048 * 8,
+    // ADD_REACTIONS = 2048 * 16,
+    // MENTION_EVERYONE = 2048 * 8,
     READ_MESSAGES = 2048 * 4,
     MANAGE_MESSAGES = 2048 * 2,
-    SEND_MESSAGES = 2048
+    SEND_MESSAGES = 2048,
   }
   export enum Voice {
-    MOVE_MEMBERS = 32768 * 8,
-    MUTE_MEMBERS = 32768 * 4,
+    // MOVE_MEMBERS = 32768 * 8,
+    // MUTE_MEMBERS = 32768 * 4,
     SPEAK = 32768 * 2,
-    CONNECT = 32768
+    CONNECT = 32768,
   }
   export const All = {
     ...General,
@@ -38,7 +38,7 @@ export namespace PermissionTypes {
     | PermissionTypes.General.CREATE_INVITE
     | PermissionTypes.Text.SEND_MESSAGES
     | PermissionTypes.Text.READ_MESSAGES
-    | PermissionTypes.Text.ADD_REACTIONS
-    | PermissionTypes.Voice.CONNECT
-    | PermissionTypes.Voice.SPEAK;
+    // | PermissionTypes.Text.ADD_REACTIONS
+    // | PermissionTypes.Voice.CONNECT
+    // | PermissionTypes.Voice.SPEAK;
 }
