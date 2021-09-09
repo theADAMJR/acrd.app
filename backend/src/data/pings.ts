d5e2899a7e7ce65a4bc4d79ad5e3bde9334bae5d
ADAMJR <adamjf123@gmail.com>
2021-09-09 12:48:39 +0100

    Fix Message Loading

--- backend/src/data/pings.ts
+++ backend/src/data/pings.ts
@@ -2,7 +2,8 @@ import { SelfUserDocument } from './models/user';
 
 export default class Pings {
   public markAsRead(user: SelfUserDocument, message: Entity.Message) {
-    user.lastReadMessageIds[message.channelId] = message.id;
+    if (user.lastReadMessageIds)
+      user.lastReadMessageIds[message.channelId] = message.id;
     return user.updateOne(user);
   }
 }
