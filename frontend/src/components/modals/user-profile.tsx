3144cf55a5ef60863b7037978aaa36a24614532d
ADAMJR <adamjf123@gmail.com>
2021-09-10 17:09:40 +0100

    Style Large Username

--- frontend/src/components/modals/user-profile.tsx
+++ frontend/src/components/modals/user-profile.tsx
@@ -1,14 +1,21 @@
 import { FunctionComponent } from 'react';
 import { useSelector } from 'react-redux';
+import Username from '../user/username';
 import Modal from './modal';
 
 const UserProfile: FunctionComponent = () => {
-  const user = useSelector((s: Store.AppState) => s.ui.activeUser);  
+  const user = useSelector((s: Store.AppState) => s.ui.activeUser); 
+  
+  
   return (user) ? (
     <Modal
-      className="p-5"
       type={UserProfile}
-      size="md">{user.username}</Modal>
+      size="md">
+      <header className="bg-bg-tertiary p-5">
+        <Username avatarSize="lg" user={user} />
+      </header>
+      <main>main body</main>
+    </Modal>
   ) : null;
 }
  
--- frontend/src/components/user/username.tsx
+++ frontend/src/components/user/username.tsx
@@ -7,9 +7,10 @@ import { useSelector } from 'react-redux';
 export interface UsernameProps {
   user: Entity.User;
   guild?: Entity.Guild;
+  avatarSize?: 'md' | 'lg';
 }
  
-const Username: React.FunctionComponent<UsernameProps> = ({ guild, user }) => {
+const Username: React.FunctionComponent<UsernameProps> = ({ guild, user, avatarSize = 'md' }) => {
   const highestRole = useSelector(getMemberHighestRole(guild?.id, user.id));
 
   const userOwnsGuild = guild?.ownerId === user.id;
@@ -18,16 +19,29 @@ const Username: React.FunctionComponent<UsernameProps> = ({ guild, user }) => {
     .padStart(4, '0');
   const isOnline = user.status === 'ONLINE';
 
+  const UserPresence = () => {
+    const blob = {
+      color: (isOnline) ? 'bg-success' : 'bg-gray-500',
+      size: (avatarSize === 'lg') ? 'h-5 w-5' : 'h-3.5 w-3.5',
+    };
+
+    return (
+      <span className="rounded-full absolute flex -right-0.5 -bottom-0.5">
+        <span
+          style={{ border: '2px solid var(--bg-secondary)' }}
+          className={`relative inline-flex rounded-full px-1 ${blob.size} ${blob.color}`} />
+      </span>
+    );
+  }
+
   return (
     <div className={`flex items-center px-2 ${!isOnline && 'opacity-50'}`}>
       <div className="relative avatar mr-2">
-        <span className="rounded-full absolute flex -right-0.5 -bottom-0.5">
-          <span
-            style={{ border: '2px solid var(--bg-secondary)' }}
-            className={`relative inline-flex rounded-full px-1 h-3.5 w-3.5 ${isOnline ? 'bg-success' : 'bg-gray-500'}`} />
-        </span>
+        <UserPresence />
         <img
-          className="select-none rounded-full w-8 h-8"
+          className={(avatarSize === 'md')
+            ? `select-none rounded-full w-8 h-8`
+            : `select-none rounded-full w-16 h-16`}
           src={`${environment.cdnURL}${user.avatarURL}`} />
       </div>
       <div className="tag leading-4">
