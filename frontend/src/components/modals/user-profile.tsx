f96af1017135f24dfb2e85f39d4d7aee8a990936
ADAMJR <adamjf123@gmail.com>
2021-09-10 17:14:07 +0100

    Add Basic Tabbing System to User Profile

--- frontend/src/components/modals/user-profile.tsx
+++ frontend/src/components/modals/user-profile.tsx
@@ -1,11 +1,18 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Username from '../user/username';
+import TabLink from '../utils/tab-link';
 import Modal from './modal';
 
 const UserProfile: FunctionComponent = () => {
-  const user = useSelector((s: Store.AppState) => s.ui.activeUser); 
-  
+  const user = useSelector((s: Store.AppState) => s.ui.activeUser);
+  const [tab, setTab] = useState('info');  
+
+  type Tab = { name: string, id: string };
+  const tabs: Tab[] = [
+    { name: 'Info', id: 'info' },
+    { name: 'Mutual Guilds', id: 'mutualGuilds' },
+  ];
   
   return (user) ? (
     <Modal
@@ -13,8 +20,18 @@ const UserProfile: FunctionComponent = () => {
       size="md">
       <header className="bg-bg-tertiary p-5">
         <Username avatarSize="lg" user={user} />
+        {tabs.map(t => (
+          <TabLink
+            tab={tab}
+            setTab={setTab}
+            id={t.id}
+            key={t.id}>{t.name}</TabLink>
+        ))}
       </header>
-      <main>main body</main>
+      <main>
+        {(tab === 'info') && 'info'}
+        {(tab === 'mutualGuilds') && 'mutual guilds'}
+      </main>
     </Modal>
   ) : null;
 }
