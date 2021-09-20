93d2c94a0ad23ecb9558d1996bd3bf5085b0e5a9
ADAMJR <adamjf123@gmail.com>
2021-09-20 13:11:17 +0100

    Add Channel Menu

--- frontend/src/components/ctx-menus/channel-menu.tsx
+++ frontend/src/components/ctx-menus/channel-menu.tsx
@@ -1,8 +1,9 @@
 import { ContextMenu, MenuItem } from 'react-contextmenu';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import usePerms from '../../hooks/use-perms';
 import { deleteChannel } from '../../store/channels';
+import DevModeMenuSection from './dev-mode-menu-section';
 
 export interface ChannelMenusProps {
   channel: Entity.Channel;
@@ -12,12 +13,13 @@ const ChannelMenu: React.FunctionComponent<ChannelMenusProps> = ({ channel }) =>
   const dispatch = useDispatch();
   const { guildId }: any = useParams();
   const perms = usePerms();
+  const devMode = useSelector((s: Store.AppState) => s.config.devMode);
 
   return (guildId) ? (
     <ContextMenu
       key={channel.id}
       id={channel.id}
-      className="bg-bg-tertiary rounded shadow w-48 p-2">
+      className="bg-bg-tertiary rounded shadow w-52 p-2">
       {perms.can('MANAGE_CHANNELS', guildId) && (
         <MenuItem
           className="danger cursor-pointer"
@@ -25,6 +27,9 @@ const ChannelMenu: React.FunctionComponent<ChannelMenusProps> = ({ channel }) =>
           <span>Delete channel</span>
         </MenuItem>
       )}
+      {devMode && <DevModeMenuSection ids={[
+        { title: 'Channel ID', id: channel.id },
+      ]} />}
     </ContextMenu>
   ) : null;
 }
