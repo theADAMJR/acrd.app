3fb868c84cd3c7cda006aae586bd8e1b79013526
ADAMJR <adamjf123@gmail.com>
2021-09-20 11:50:35 +0100

    Testing123

---
+++ frontend/src/components/utils/ui-dialog.tsx
@@ -0,0 +1,17 @@
+import { FunctionComponent } from 'react';
+
+interface UIDialogProps {
+  
+}
+ 
+const UIDialog: FunctionComponent<UIDialogProps> = () => {
+  const use
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}
+ 
+export default UIDialog;
