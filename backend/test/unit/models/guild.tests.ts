407adee3cad1d9d058576a0d77bdd11741f40b3d
ADAMJR <adamjf123@gmail.com>
2021-09-25 15:46:48 +0100

    Fix Guild Model Tests

--- backend/test/unit/models/guild.tests.ts
+++ backend/test/unit/models/guild.tests.ts
@@ -5,15 +5,11 @@ import { Guild } from '../../../src/data/models/guild';
 
 test(createGuild, () => {
   given().expect(true);
-  given({ channels: longArray(251) }).expect('Channel limit reached');
-  given({ channels: longArray(250) }).expect(true);
   given({ name: '' }).expect('Name is required');
   given({ name: 'Mock Guild' }).expect(true);
   given({ name: longString(33) }).expect('Name is too long');
   given({ ownerId: '123' }).expect('Invalid Snowflake ID');
   given({ ownerId: generateSnowflake() }).expect(true);
-  given({ roles: [] }).expect('Guild must have at least one role');
-  given({ roles: [generateSnowflake()] }).expect(true);
 });
 
 function createGuild(guild: any) {
@@ -21,12 +17,6 @@ function createGuild(guild: any) {
     _id: generateSnowflake(),
     name: 'Mock Guild',
     ownerId: generateSnowflake(),
-    roles: [generateSnowflake()],
-    channels: [
-      generateSnowflake(),
-      generateSnowflake(),
-    ],
-    members: [generateSnowflake()],
     ...guild
   }).validateSync();
 
