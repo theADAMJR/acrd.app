7eac7bf941e7c9a0e91b0017903fb43ae012afff
ADAMJR <adamjf123@gmail.com>
2021-09-29 13:01:46 +0100

    Add Markdown to Messages

--- frontend/src/components/channel/message/message.tsx
+++ frontend/src/components/channel/message/message.tsx
@@ -25,10 +25,32 @@ const Message: React.FunctionComponent<MessageProps> = ({ message }: MessageProp
   const editingMessageId = useSelector((s: Store.AppState) => s.ui.editingMessageId);
   const createdAt = new Date(message.createdAt);
 
+  const patterns = {
+    boldItalic: /\*\*\*(.*?)\*\*\*/gs,
+    bold: /\*\*(.*?)\*\*/gs,
+    italic: /\*(.*?)\*|_(.*?)_/gs,
+    underline: /__(.*?)__/gs,
+    underlineItalics: /__\*(.*?)\*__/gs,
+    underlineBold: /__\*\*(.*?)\*\*__/gs,
+    underlineBoldItalics: /__\*\*\*(.*?)\*\*\*__/gs,
+    strikethrough: /~~(.*?)~~/gs,
+    codeMultiline: /```(.*?)```/gs,
+    codeLine: /`(.*?)`/gs,
+    blockQuoteMultipleLine: /(.*?)`/gs,
+    blockQuoteOneLine: /`(.*?)`/gs,
+  }
+
   const format = (content: string) => content
-    .replace(/\*\*\*(.*?)\*\*\*|___(.*?)___/gs, '<strong><em>$1$2</em></strong>')
-    .replace(/\*\*(.*?)\*\*|__(.*?)__/gs, '<strong>$1$2</strong>')
-    .replace(/\*(.*?)\*|_(.*?)_/gs, '<em>$1$2</em>')
+    .replace(patterns.boldItalic, '<strong><em>$1</em></strong>')
+    .replace(patterns.bold, '<strong>$1</strong>')
+    .replace(patterns.italic, '<em>$1$2</em>')
+    .replace(patterns.underline, '<u>$1</u>')
+    .replace(patterns.underlineItalics, '<u><em>$1</em></u>')
+    .replace(patterns.underlineBold, '<u><strong>$1</strong></u>')
+    .replace(patterns.underlineBoldItalics, '<u><strong><em>$1</strong></em></u>')
+    .replace(patterns.strikethrough, '<del>$1</del>')
+    .replace(patterns.codeMultiline, '<pre><code>$1</code></pre>')
+    .replace(patterns.codeLine, '<code>$1</code>')
 
   const isExtra = () => {
     const i = messages.findIndex(m => m.id === message.id);
