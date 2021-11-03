72e339aa84a834028f4cf3687fcb4fa8e034741b
ADAMJR <adamjf123@gmail.com>
2021-11-03 16:44:53 +0000

    Backend: Proper 404 Error Message for Assets

--- backend/src/rest/functions/apply-error-handling.ts
+++ backend/src/rest/functions/apply-error-handling.ts
@@ -3,11 +3,10 @@ import { Application } from 'express-serve-static-core';
 import { APIError } from '../modules/api-error';
 
 export default (app: Application, prefix: string) => {
-  app.all(`${prefix}/*`, (req, res, next) => next(new APIError(404)));
+  app.all(`/assets/*`, (req, res, next) => next(new APIError(404)));
+  app.all(`/${prefix}/*`, (req, res, next) => next(new APIError(404)));
 
-  app.use(`/`, () => {
-    throw new TypeError('Invalid API version number');
-  });
+  app.use(`/`, () => { throw new TypeError('Invalid API version number') });
   
   app.use((error: APIError, req: Request, res: Response, next: NextFunction) => {
     if (res.headersSent)
