12e5c647729a8e01031786a82a7796f1a7229f7c
ADAMJR <adamjf123@gmail.com>
2021-11-04 17:14:29 +0000

    Backend: Auto Exit Tests for GH Actions

--- backend/test/test.ts
+++ backend/test/test.ts
@@ -37,43 +37,43 @@ console.log(`${space(48 * 3)}TESTS${space(54 * 2)}`.bgWhite.black);
     execSync(`kill -9 $(lsof -i :${process.env.PORT} | tail -n 1 | cut -d ' ' -f5) 2>> /dev/null`);
   } catch {}
 
-  // import('./integration/routes/auth-routes.tests');
-  // import('./integration/routes/invites-routes.tests');
-  // import('./integration/routes/guilds-routes.tests');
-  // import('./integration/routes/channel-routes.tests');
+  import('./integration/routes/auth-routes.tests');
+  import('./integration/routes/invites-routes.tests');
+  import('./integration/routes/guilds-routes.tests');
+  import('./integration/routes/channel-routes.tests');
 
-  // import('./integration/ws/channel-create.tests');
-  // // TODO: import('./integration/ws/channel-delete.tests');
-  // // TODO: import('./integration/ws/channel-update.tests');
-  // import('./integration/ws/guild-member-add.tests'); //fail
-  // import('./integration/ws/guild-member-remove.tests'); //fail
-  // import('./integration/ws/guild-member-update.tests'); // fail
-  // import('./integration/ws/guild-create.tests'); // fail
-  // import('./integration/ws/guild-delete.tests'); // fail
-  // import('./integration/ws/guild-update.tests'); // fail
-  // import('./integration/ws/invite-create.tests'); //fail
-  // import('./integration/ws/invite-delete.tests'); //fail
-  // import('./integration/ws/message-create.tests'); //fail
-  // import('./integration/ws/message-update.tests'); // fail
-  // import('./integration/ws/message-delete.tests'); // fail
-  // import('./integration/ws/ready.tests');
-  // import('./integration/ws/user-update.tests');
-  // import('./integration/ws/ws-guard.tests');
-  // import('./integration/data/roles.tests');
+  import('./integration/ws/channel-create.tests');
+  // TODO: import('./integration/ws/channel-delete.tests');
+  // TODO: import('./integration/ws/channel-update.tests');
+  import('./integration/ws/guild-member-add.tests'); //fail
+  import('./integration/ws/guild-member-remove.tests'); //fail
+  import('./integration/ws/guild-member-update.tests'); // fail
+  import('./integration/ws/guild-create.tests'); // fail
+  import('./integration/ws/guild-delete.tests'); // fail
+  import('./integration/ws/guild-update.tests'); // fail
+  import('./integration/ws/invite-create.tests'); //fail
+  import('./integration/ws/invite-delete.tests'); //fail
+  import('./integration/ws/message-create.tests'); //fail
+  import('./integration/ws/message-update.tests'); // fail
+  import('./integration/ws/message-delete.tests'); // fail
+  import('./integration/ws/ready.tests');
+  import('./integration/ws/user-update.tests');
+  import('./integration/ws/ws-guard.tests');
+  import('./integration/data/roles.tests');
 
-  // import('./unit/models/app.tests');
-  // import('./unit/models/channel.tests');
-  // import('./unit/models/guild.tests');
-  // import('./unit/models/guild-member.tests');
-  // import('./unit/models/invite.tests');
-  // import('./unit/models/message.tests');
-  // import('./unit/models/role.tests');
-  // import('./unit/models/user.tests');
-  // import('./unit/snowflake-entity.tests');
-  // import('./unit/ws/ws-cooldowns.tests');
+  import('./unit/models/app.tests');
+  import('./unit/models/channel.tests');
+  import('./unit/models/guild.tests');
+  import('./unit/models/guild-member.tests');
+  import('./unit/models/invite.tests');
+  import('./unit/models/message.tests');
+  import('./unit/models/role.tests');
+  import('./unit/models/user.tests');
+  import('./unit/snowflake-entity.tests');
+  import('./unit/ws/ws-cooldowns.tests');
 })();
 
 // needs to be here, or tests won't run
 describe('oh', () => it('frick', () => expect(true).to.be.true));
 describe('oh', () => it('no', () => expect(false).to.be.true));
-after(() => process.exit(0));
+// after(() => process.exit(0));
