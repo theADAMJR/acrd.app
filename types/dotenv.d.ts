80a1c59748376d9a144517b91e85566b4fa247b4
ADAMJR <adamjf123@gmail.com>
2021-11-05 15:19:51 +0000

    GH Actions: Add .env when Starting Backend

--- types/dotenv.d.ts
+++ types/dotenv.d.ts
@@ -1,13 +1,11 @@
 declare global {
   namespace NodeJS {
     export interface ProcessEnv {
-      API_URL: string;
       EMAIL_ADDRESS: string;
       EMAIL_PASSWORD: string;
       MONGO_URI: string;
       NODE_ENV: 'dev' | 'prod';
       PORT: string;
-      ROOT_ENDPOINT: string;
       WEBSITE_URL: string;
     }
   }
